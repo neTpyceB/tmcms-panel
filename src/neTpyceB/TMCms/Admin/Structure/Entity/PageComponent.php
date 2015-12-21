5187fe0b679a05a0a54ca27bf086bd2335a1fd99
neTpyceB <pvman@inbox.ru>
2015-12-21 12:59:59 +0200

    Page components objects

---
+++ src/neTpyceB/TMCms/Admin/Structure/Entity/PageComponent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace neTpyceB\TMCms\Admin\Structure\Entity;
+
+use neTpyceB\TMCms\Orm\Entity;
+
+/**
+ * Class PageComponent
+ * @package neTpyceB\TMCms\Admin\Structure\Entity
+ */
+class PageComponent extends Entity
+{
+    protected $db_table = 'cms_pages_components';
+}
---
+++ src/neTpyceB/TMCms/Admin/Structure/Entity/PageComponentRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace neTpyceB\TMCms\Admin\Structure\Entity;
+
+use neTpyceB\TMCms\Orm\EntityRepository;
+
+/**
+ * Class PageComponentRepository
+ * @package neTpyceB\TMCms\Admin\Structure\Entity
+ *
+ * @method $this setWherePageId(int $page_id)
+ */
+class PageComponentRepository extends EntityRepository
+{
+    protected $db_table = 'cms_pages_components';
+}
