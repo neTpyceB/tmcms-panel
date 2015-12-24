0da735c75b41996202a915c71804e0bd6381be84
neTpyceB <pvman@inbox.ru>
2015-12-24 13:26:16 +0200

    Admin log-in attempts repo

---
+++ src/neTpyceB/TMCms/Admin/Guest/Entity/AdminUsersAttemptsEntityRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace neTpyceB\TMCms\Admin\Guest\Entity;
+
+use neTpyceB\TMCms\Orm\EntityRepository;
+
+/**
+ * Class AdminUsersAttemptsRepository
+ * @package neTpyceB\TMCms\Admin\Guest\Entity
+ *
+ * @method setWhereIp(string $ip)
+ */
+class AdminUsersAttemptsEntityRepository extends EntityRepository
+{
+    protected $db_table = 'cms_users_attempts';
+}
