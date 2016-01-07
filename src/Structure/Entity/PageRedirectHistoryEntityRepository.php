90ff1995cd1b3a1010e9232aa40145a03f0bcd1c
neTpyceB <pvman@inbox.ru>
2016-01-07 13:45:59 +0200

    DB Structure for component rename history

--- src/Structure/Entity/PageRedirectHistoryEntityRepository.php
+++ src/Structure/Entity/PageRedirectHistoryEntityRepository.php
@@ -7,4 +7,38 @@ use TMCms\Orm\EntityRepository;
 class PageRedirectHistoryEntityRepository extends EntityRepository
 {
     protected $db_table = 'cms_pages_redirect_history';
+    protected $table_structure = [
+        'fields' => [
+            'page_id' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+            'user_id' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+            'component' => [
+                'type' => 'varchar',
+            ],
+            'data' => [
+                'type' => 'mediumtext',
+            ],
+            'ts' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+            'version' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+        ],
+        'indexes' => [
+            'page_id' => [
+                'type' => 'key',
+            ],
+            'user_id' => [
+                'type' => 'key',
+            ],
+        ],
+    ];
 }
