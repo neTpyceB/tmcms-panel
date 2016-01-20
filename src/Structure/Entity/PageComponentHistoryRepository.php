7811481de0d2c50e96d7e4e860ebc074449cb4d2
neTpyceB <pvman@inbox.ru>
2016-01-20 15:50:45 +0200

    Table structures for Components

--- src/Structure/Entity/PageComponentHistoryRepository.php
+++ src/Structure/Entity/PageComponentHistoryRepository.php
@@ -14,4 +14,28 @@ use TMCms\Orm\EntityRepository;
 class PageComponentHistoryRepository extends EntityRepository
 {
     protected $db_table = 'cms_pages_components_history';
+    protected $table_structure = [
+        'fields' => [
+            'page_id' => [
+                'type' => 'index'
+            ],
+            'user_id' => [
+                'type' => 'index'
+            ],
+            'version' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+            'component' => [
+                'type' => 'varchar',
+            ],
+            'data' => [
+                'type' => 'mediumtext',
+            ],
+            'ts' => [
+                'type' => 'int',
+                'unsigned' => true,
+            ],
+        ],
+    ];
 }
