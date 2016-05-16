ebe1510fdc7e84d5300de8fbe2e29061002ea438
neTpyceB <pvman@inbox.ru>
2016-05-17 01:05:25 +0300

    Template odd fields

--- src/Structure/Entity/PageTemplateEntityRepository.php
+++ src/Structure/Entity/PageTemplateEntityRepository.php
@@ -9,14 +9,8 @@ class PageTemplateEntityRepository extends EntityRepository
     protected $db_table = 'cms_pages_templates';
     protected $table_structure = [
         'fields' => [
-            'title' => [
-                'type' => 'varchar',
-            ],
             'file' => [
                 'type' => 'varchar',
-            ],
-            'comment' => [
-                'type' => 'text',
             ],
         ],
     ];
