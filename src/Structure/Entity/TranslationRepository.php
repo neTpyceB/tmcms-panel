80e907bc2632ff69627cc683347179ec494544cc
neTpyceB <pvman@inbox.ru>
2017-01-07 00:12:31 +0200

    Cross-reference in translations for Entities

--- src/Structure/Entity/TranslationRepository.php
+++ src/Structure/Entity/TranslationRepository.php
@@ -4,12 +4,24 @@ namespace TMCms\Admin\Structure\Entity;
 
 use TMCms\Orm\EntityRepository;
 
+/**
+ * Class TranslationRepository
+ * @package TMCms\Admin\Structure\Entity
+ *
+ * @method $this setEntity(string $entity)
+ * @method $this setEntityId(int $entity_id)
+ */
 class TranslationRepository extends EntityRepository
 {
     protected $db_table = 'cms_translations';
     protected $table_structure = [
         'fields' => [
-
+            'entity' => [
+                'type' => 'varchar',
+            ],
+            'entity_id' => [
+                'type' => 'ts',
+            ],
         ],
     ];
 }
