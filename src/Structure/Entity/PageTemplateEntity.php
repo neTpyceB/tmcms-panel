b6f77a23009a4fde4d131d043b63aa2966f13bd1
neTpyceB <pvman@inbox.ru>
2017-02-12 18:20:58 +0200

    PHPDoc added

--- src/Structure/Entity/PageTemplateEntity.php
+++ src/Structure/Entity/PageTemplateEntity.php
@@ -4,6 +4,12 @@ namespace TMCms\Admin\Structure\Entity;
 
 use TMCms\Orm\Entity;
 
+/**
+ * Class PageTemplateEntity
+ * @package TMCms\Admin\Structure\Entity
+ *
+ * @method $this setFile(string $file)
+ */
 class PageTemplateEntity extends Entity
 {
     protected $db_table = 'cms_pages_templates';
