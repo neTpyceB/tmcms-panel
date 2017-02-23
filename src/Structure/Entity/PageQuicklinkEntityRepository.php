f51b2b75814e67fcd041f306b8091c6a7073e3ee
neTpyceB <pvman@inbox.ru>
2017-02-23 18:35:45 +0200

    PHPDoc added

--- src/Structure/Entity/PageQuicklinkEntityRepository.php
+++ src/Structure/Entity/PageQuicklinkEntityRepository.php
@@ -4,6 +4,13 @@ namespace TMCms\Admin\Structure\Entity;
 
 use TMCms\Orm\EntityRepository;
 
+/**
+ * Class PageQuicklinkEntityRepository
+ * @package TMCms\Admin\Structure\Entity
+ *
+ * @method $this setWhereName(string $name)
+ * @method $this setWhereSearchword(int $flag)
+ */
 class PageQuicklinkEntityRepository extends EntityRepository
 {
     protected $db_table = 'cms_pages_quicklinks';
