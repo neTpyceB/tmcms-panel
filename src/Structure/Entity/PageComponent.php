72686764eb151163b070d2f4142e2c9c18485710
neTpyceB <pvman@inbox.ru>
2017-02-26 14:11:05 +0200

    PHPDoc added

--- src/Structure/Entity/PageComponent.php
+++ src/Structure/Entity/PageComponent.php
@@ -11,6 +11,10 @@ use TMCms\Orm\Entity;
  * @method string getComponent()
  * @method string getData()
  * @method int getPageId()
+ *
+ * @method $this setComponent(string $component)
+ * @method $this setData(string $data_content)
+ * @method $this setPageId(int $page_id)
  */
 class PageComponent extends Entity
 {
