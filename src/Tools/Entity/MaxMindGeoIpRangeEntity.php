165d8e17aecc3343be92aaba63d4e70896ef562b
neTpyceB <pvman@inbox.ru>
2017-07-20 23:38:20 +0300

    PHPDoc added

--- src/Tools/Entity/MaxMindGeoIpRangeEntity.php
+++ src/Tools/Entity/MaxMindGeoIpRangeEntity.php
@@ -5,6 +5,12 @@ namespace TMCms\Admin\Tools\Entity;
 
 use TMCms\Orm\Entity;
 
+/**
+ * Class MaxMindGeoIpRangeEntity
+ * @package TMCms\Admin\Tools\Entity
+ *
+ * @method string getCode()
+ */
 class MaxMindGeoIpRangeEntity extends Entity
 {
     protected $db_table = 'cms_maxmind_geoip_r';
