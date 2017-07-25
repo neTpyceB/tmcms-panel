884aa4a2af7087fa6c26923fccbb0343eb62becc
neTpyceB <pvman@inbox.ru>
2017-07-25 15:38:25 +0300

    Removed possibility to save link to enity in translations

--- src/Structure/Entity/TranslationRepository.php
+++ src/Structure/Entity/TranslationRepository.php
@@ -7,9 +7,6 @@ use TMCms\Orm\EntityRepository;
 /**
  * Class TranslationRepository
  * @package TMCms\Admin\Structure\Entity
- *
- * @method $this setEntity(string $entity)
- * @method $this setEntityId(int $entity_id)
  */
 class TranslationRepository extends EntityRepository
 {
