d092db1b7a71e8b3e5cd93dad79903351b8b2b5f
neTpyceB <pvman@inbox.ru>
2017-08-07 22:23:07 +0300

    Removed ols protocol, code uses curl

--- src/Tools/Pages/_submit_structure_xml.php
+++ src/Tools/Pages/_submit_structure_xml.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 use TMCms\Admin\Messages;
 use TMCms\Cache\Cacher;
 use TMCms\Log\App;
-use TMCms\Network\HttpProtocol;
 
 defined('INC') or exit;
 
 $last_ts = 0;
-if (file_exists(DIR_BASE . '/sitemap.gz') && HttpProtocol::is_status_200(HttpProtocol::extract_headers(HttpProtocol::get('http://www.google.com/ping?sitemap=http://' . CFG_DOMAIN . '/sitemap.xml')))) {
-    $last_ts = NOW;
+if (file_exists(DIR_BASE . '/sitemap.gz')) {
+    $url = 'http://www.google.com/ping?sitemap=http://' . CFG_DOMAIN . '/sitemap.xml';
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_exec($ch);
+
+    $return_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    if ('200' === $return_code) {
+        $last_ts = NOW;
+    }
 }
 
 Cacher::getInstance()->getDefaultCacher()->set('cms_tools_submit_structure_xml', $last_ts);
