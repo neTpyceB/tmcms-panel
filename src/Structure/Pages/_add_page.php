b9b97d8503bc4d304c65f15bdff594d4d9b67a51
Vadims Petrusevs <pvman@inbox.ru>
2018-05-26 11:17:01 +0200

    Create page duplicateswhen creating, for all other languages, updated

--- src/Structure/Pages/_add_page.php
+++ src/Structure/Pages/_add_page.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 use TMCms\Admin\Messages;
 use TMCms\Admin\Structure\CmsStructure;
 use TMCms\Admin\Structure\Entity\PageEntity;
+use TMCms\Admin\Structure\Entity\PageEntityRepository;
 use TMCms\Config\Settings;
 use TMCms\DB\SQL;
 use TMCms\Log\App;
+use TMCms\Routing\Languages;
 use TMCms\Routing\Structure;
 use TMCms\Strings\UID;
 
@@ -40,33 +42,52 @@ if (!$_POST['string_label']) {
 
 $page = new PageEntity();
 $page->loadDataFromArray([
-    'template_id'     => (int)$_POST['template_id'],
-    'pid'             => $pid,
-    'location'        => $location,
-    'title'           => $_POST['title'],
-    'order'           => SQL::getNextOrder($page->getDbTableName(), 'order', 'pid', $pid),
-    'keywords'        => $_POST['keywords'],
-    'description'     => $_POST['description'],
+    'template_id' => (int)$_POST['template_id'],
+    'pid' => $pid,
+    'location' => $location,
+    'title' => $_POST['title'],
+    'order' => SQL::getNextOrder($page->getDbTableName(), 'order', 'pid', $pid),
+    'keywords' => $_POST['keywords'],
+    'description' => $_POST['description'],
     'transparent_get' => (int)isset($_POST['transparent_get']),
-    'string_label'    => $_POST['string_label'],
-    'menu_name'       => $_POST['menu_name'],
-    'redirect_url'    => $redirect_url,
-    'html_file'       => $_POST['html_file'],
-    'go_level_down'   => (int)isset($_POST['go_level_down']),
-    'lastmod_ts'      => NOW,
+    'string_label' => $_POST['string_label'],
+    'menu_name' => $_POST['menu_name'],
+    'redirect_url' => $redirect_url,
+    'html_file' => $_POST['html_file'],
+    'go_level_down' => (int)isset($_POST['go_level_down']),
+    'lastmod_ts' => NOW,
 ]);
 $page->save();
 
 $id = $page->getId();
 
 // Duplicate page for other languages if required
-if ($pid && isset($_POST['duplicates'])) {
+if ($pid && !empty($_POST['duplicates'])) {
     $pid_page = new PageEntity($pid);
-    $pid_location = $pid_page->getLocation();
-    foreach ($_POST['duplicates'] as $lng => $flag) {
-        $lng_parent_id = Structure::getIdByLabel($pid_location, $lng);
-        if ($lng_parent_id) {
-            CmsStructure::getInstance()->copy_pages($id, $lng_parent_id);
+    $pid_label = $pid_page->getStringLabel();
+    if ($pid_label === LNG) {
+        // Copy by language - because each language has not the same label string
+        foreach (Languages::getPairs() as $lng => $lng_full) {
+            if ($lng === LNG) {
+                // Skip current - already created
+                continue;
+            }
+
+            $lng_parent_id = Structure::getIdByPath($lng);
+            if ($lng_parent_id) {
+                // If not exists - create a new page
+                if (!PageEntityRepository::findOneEntityByCriteria(['pid' => $lng_parent_id, 'location' => $page->getLocation()])) {
+                    CmsStructure::getInstance()->copy_pages($id, $lng_parent_id);
+                }
+            }
+        }
+    } else {
+        // Copy by same parent label string
+        foreach ($_POST['duplicates'] as $lng => $flag) {
+            $lng_parent_id = Structure::getIdByLabel($pid_label, $lng);
+            if ($lng_parent_id) {
+                CmsStructure::getInstance()->copy_pages($id, $lng_parent_id);
+            }
         }
     }
 }
